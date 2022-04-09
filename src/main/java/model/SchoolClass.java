8079f73b98247b656c4e3490ff585f8cb8070d63
milan-codes <github@milancodes.io>
2022-04-09 13:43:42 +0200

    created class model

---
+++ src/main/java/model/SchoolClass.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.UUID;
+
+public class SchoolClass {
+    private final UUID id = UUID.randomUUID();
+    private String name;
+    private String teacher;
+    private String classroom;
+    private String day;
+    private String time;
+
+    public SchoolClass(String name, String teacher, String classRoom, String day, String time) {
+        this.name = name;
+        this.teacher = teacher;
+        this.classroom = classRoom;
+        this.day = day;
+        this.time = time;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
